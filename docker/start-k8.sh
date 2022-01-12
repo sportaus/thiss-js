66096d42925a6d83115f123015c25fa4af9e8470
Matthew Lawler <raisty@gmail.com>
2022-01-12 14:27:08 +1000

    updated and added new style support for chrome

--- docker/start-k8.sh
+++ docker/start-k8.sh
@@ -70,7 +70,8 @@ server {
         root   /usr/share/nginx/html;
         index  index.html index.htm;
         add_header 'Access-Control-Allow-Origin' '*' always;
-        add_header X-Frame-Options ALLOW-FROM ${AUTH_SERVER_URL};
+        add_header 'X-Frame-Options' "ALLOW-FROM ${AUTH_SERVER_URL}";
+        add_header 'Content-Security-Policy' "frame-ancestors ${AUTH_SERVER_URL}";
         try_files \$uri \$uri/index.html \$uri.html;
     }
 }
